ac0daeb37f2fc56c953059f60c37ff91c5b0c823
Neythan Levanest <43857977+NeythLevanest@users.noreply.github.com>
2021-10-03 17:40:57 -0500

    Pruebas para solucionar problemas cors

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,4 +1,4 @@
-const URL_ROOT = 'https://power.larc.nasa.gov/api/'
+const URL_ROOT = '/api/'
 const URL_ROOT_AUTH = 'http://localhost:8000'
 const URL_SERVICIOS = {
     root: URL_ROOT,
--- src/app/services/request-services.service.ts
+++ src/app/services/request-services.service.ts
@@ -24,15 +24,16 @@ export class RequestServicesService {
     let httpOptions = {
       headers: new HttpHeaders({
       'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Origin': 'https://power.larc.nasa.gov',
       'Access-Control-Allow-Headers': 'Content-Type',
       'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,DELETE,PUT',
       'Authorization': 'Bearer szdp79a2kz4wh4frjzuqu4sz6qeth8m3',
       })
     }
-    let url=URL_SERVICIOS.root+"temporal/"+frecuencia+"/point?parameters=ALLSKY_SFC_SW_DWN,ALLSKY_KT,T2M&community=RE&longitude="+long+"&latitude="+lat+"&start="+startDate+"&end="+endDate+"&format=JSON"
+    //let url="/api/temporal/"+frecuencia+"/point?parameters=ALLSKY_SFC_SW_DWN,ALLSKY_KT,T2M&community=RE&longitude="+long+"&latitude="+lat+"&start="+startDate+"&end="+endDate+"&format=JSON"
+    let url="https://power.larc.nasa.gov/api/temporal/"+frecuencia+"/point?parameters=ALLSKY_SFC_SW_DWN,ALLSKY_KT,T2M&community=RE&longitude="+long+"&latitude="+lat+"&start="+startDate+"&end="+endDate+"&format=JSON"
     console.log(url);
-    return this.http.get(url);
+    return this.http.get(url, httpOptions);
 
   }
 
